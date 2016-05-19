685da98c017b6cb751843202837796e07e73ca88
殷豪 <@hao>
2016-05-19 22:54:22 +0800

    jdk 8:lamada表达式

--- demol/src/main/java/com/beautiful/demol/App.java
+++ demol/src/main/java/com/beautiful/demol/App.java
@@ -1,5 +1,6 @@
 package com.beautiful.demol;
 
+
 /**
  * Hello world!
  *
@@ -9,5 +10,26 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        new Thread(){
+        	@Override
+        	public void run() {
+        		super.run();
+        		System.out.println("thread 1");
+        	}
+        }.start();
+        
+        new Thread(new Runnable() {
+			
+        	@Override
+			public void run() {
+				System.out.println("thread 2");
+			}
+		}).start();
+        
+        new Thread(()-> {
+        	int i=1+1;
+        	System.out.println("thread 3"+";"+i);
+        	}).start();
     }
 }
