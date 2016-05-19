43284f337e2bebd291b4011135a46a45c3c6f9ab
殷豪 <@hao>
2016-05-19 23:52:56 +0800

    spring最新示例

---
+++ demol/src/main/java/MessageService/Application.java
@@ -0,0 +1,28 @@
+package MessageService;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class Application {
+
+	@Bean
+    MessageService mockMessageService() {
+		//匿名内部类
+        return new MessageService() {
+            public String getMessage() {
+              return "Hello World!2";
+            }
+        };
+    }
+
+  public static void main(String[] args) {
+      ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+      MessagePrinter printer = context.getBean(MessagePrinter.class);
+      printer.printMessage();
+  }
+}
---
+++ demol/src/main/java/MessageService/MessageService.java
@@ -0,0 +1,5 @@
+package MessageService;
+
+public interface MessageService {
+    String getMessage();
+}
